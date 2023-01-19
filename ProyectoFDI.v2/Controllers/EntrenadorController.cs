5fc17b7ad1278fcba8ca23966ee6638e3d46ed2e
Erick Pineda <eapinedac@utn.edu.ec>
2023-01-19 07:23:12 -0500

    Controlador y vistas Entrenador

---
+++ ProyectoFDI.v2/Controllers/EntrenadorController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProyectoFDI.v2.Code;
+using ProyectoFDI.v2.Models;
+
+namespace ProyectoFDI.v2.Controllers
+{
+    public class EntrenadorController : Controller
+    {
+        private string apiUrl;
+
+        public EntrenadorController(IConfiguration configuration)
+        {
+            apiUrl = configuration["urlBase"].ToString() + "Entrenador/";
+        }
+        // GET: EntrenadorController
+        public ActionResult Index(string? searchFor)
+        {
+            ViewBag.SearchFor = "" + searchFor;
+
+            if (string.IsNullOrWhiteSpace(searchFor))
+            {
+                return View(APIConsumer<Entrenador>.Select(apiUrl));
+            }
+            else
+            {
+                return View(APIConsumer<Entrenador>.Select_SearchFor(apiUrl, searchFor));
+            }
+        }
+        private List<SelectListItem> listaProvincias()
+        {
+            var provincias = APIConsumer<Provincium>.Select(apiUrl.Replace("Entrenador", "Provincia"));
+            var lista = provincias.Select(f => new SelectListItem
+            {
+                Value = f.IdPro.ToString(),
+                Text = f.NombrePro
+            }).ToList();
+
+            return lista;
+        }
+        private List<SelectListItem> listaDeportistas()
+        {
+            var deportistas = APIConsumer<Deportistum>.Select(apiUrl.Replace("Entrenador", "Deportistas"));
+            var lista = deportistas.Select(f => new SelectListItem
+            {
+                Value = f.IdDep.ToString(),
+                Text = f.NombresDep
+            }).ToList();
+
+            return lista;
+        }
+        private List<SelectListItem> listaUsuarios()
+        {
+            var usuarios = APIConsumer<Usuario>.Select(apiUrl.Replace("Entrenador", "Usuario"));
+            var lista = usuarios.Select(f => new SelectListItem
+            {
+                Value = f.IdUsu.ToString(),
+                Text = f.NombreUsu
+            }).ToList();
+
+            return lista;
+        }
+
+        // GET: EntrenadorController/Details/5
+        public ActionResult Details(int id)
+        {
+            var data = APIConsumer<Entrenador>.SelectOne(apiUrl + id.ToString());
+            return View(data);
+        }
+
+        // GET: EntrenadorController/Create
+        public ActionResult Create()
+        {
+            ViewBag.ListaDeportistas = listaDeportistas();
+            ViewBag.ListaProvincias = listaProvincias();
+            ViewBag.ListaUsuarios = listaUsuarios();
+            return View();
+        }
+
+        // POST: EntrenadorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Entrenador entrenador)
+        {
+            try
+            {
+                APIConsumer<Entrenador>.Insert(apiUrl, entrenador);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(entrenador);
+            }
+        }
+
+        // GET: EntrenadorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.ListaDeportistas = listaDeportistas();
+            ViewBag.ListaProvincias = listaProvincias();
+            ViewBag.ListaUsuarios = listaUsuarios();
+            var data = APIConsumer<Entrenador>.SelectOne(apiUrl + id.ToString());
+            return View(data);
+        }
+
+        // POST: EntrenadorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Models.Entrenador entrenador)
+        {
+            try
+            {
+                APIConsumer<Entrenador>.Update(apiUrl + id.ToString(), entrenador);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(entrenador);
+            }
+        }
+
+        // GET: EntrenadorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var data = APIConsumer<Entrenador>.SelectOne(apiUrl + id.ToString());
+            return View(data);
+        }
+
+        // POST: EntrenadorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Models.Entrenador entrenador)
+        {
+            try
+            {
+                APIConsumer<Entrenador>.Delete(apiUrl + id.ToString());
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(entrenador);
+            }
+        }
+    }
+}
