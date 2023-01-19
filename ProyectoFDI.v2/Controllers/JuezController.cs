c80e1b473441f2ab413b0b6a58f08e54292138f6
JorgePazos-git <jrpazosm@utn.edu.ec>
2023-01-18 21:42:51 -0500

    Vistas de juez, club, cambios en el menu y agregacion de imagenes

---
+++ ProyectoFDI.v2/Controllers/JuezController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProyectoFDI.v2.Code;
+using ProyectoFDI.v2.Models;
+using System.Data;
+
+namespace ProyectoFDI.v2.Controllers
+{
+    public class JuezController : Controller
+    {
+        private string apiUrl;
+
+        public JuezController(IConfiguration configuration)
+        {
+            apiUrl = configuration["urlBase"].ToString() + "Juez/";
+        }
+
+        [Authorize(Roles = "Administrador")]
+        // GET: JuezController
+        public ActionResult Index(string? searchFor)
+        {
+            ViewBag.SearchFor = "" + searchFor;
+
+            if (string.IsNullOrWhiteSpace(searchFor))
+            {
+                return View(APIConsumer<Juez>.Select(apiUrl));
+            }
+            else
+            {
+                return View(APIConsumer<Juez>.Select_SearchFor(apiUrl, searchFor));
+            }
+        }
+
+        private List<SelectListItem> listaEstados()
+        {
+            var roles = new List<SelectListItem>();
+            roles.Add(new SelectListItem { Text = "Si", Value = "true" });
+            roles.Add(new SelectListItem { Text = "No", Value = "false" });
+            return roles;
+        }
+
+        private List<SelectListItem> listaProvincias()
+        {
+            var provincias = APIConsumer<Provincium>.Select(apiUrl.Replace("Juez", "Provincia"));
+            var lista = provincias.Select(f => new SelectListItem
+            {
+                Value = f.IdPro.ToString(),
+                Text = f.NombrePro
+            }).ToList();
+
+            return lista;
+        }
+
+        private List<SelectListItem> listaUsuarios()
+        {
+            var usuarios = APIConsumer<Usuario>.Select(apiUrl.Replace("Juez", "Usuario"));
+            var lista = usuarios.Select(f => new SelectListItem
+            {
+                Value = f.IdUsu.ToString(),
+                Text = f.NombreUsu
+            }).ToList();
+
+            return lista;
+        }
+
+        [Authorize(Roles = "Administrador")]
+        // GET: JuezController/Details/5
+        public ActionResult Details(int id)
+        {
+            var data = APIConsumer<Juez>.SelectOne(apiUrl + id.ToString());
+            return View(data);
+        }
+
+        [Authorize(Roles = "Administrador")]
+        // GET: JuezController/Create
+        public ActionResult Create()
+        {
+            ViewBag.ListaProvincias = listaProvincias();
+            ViewBag.ListaUsuarios = listaUsuarios();
+            ViewBag.ListaEstados = listaEstados();
+            return View();
+        }
+
+        [Authorize(Roles = "Administrador")]
+        // POST: JuezController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Juez juez)
+        {
+            try
+            {
+                APIConsumer<Juez>.Insert(apiUrl, juez);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(juez);
+            }
+        }
+
+        [Authorize(Roles = "Administrador")]
+        // GET: JuezController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.ListaProvincias = listaProvincias();
+            ViewBag.ListaUsuarios = listaUsuarios();
+            ViewBag.ListaEstados = listaEstados();
+            var data = APIConsumer<Juez>.SelectOne(apiUrl + id.ToString());
+            return View(data);
+        }
+
+        [Authorize(Roles = "Administrador")]
+        // POST: JuezController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Juez juez)
+        {
+            try
+            {
+                APIConsumer<Juez>.Update(apiUrl + id.ToString(), juez);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(juez);
+            }
+        }
+        
+        [Authorize(Roles = "Administrador")]
+        // GET: JuezController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var data = APIConsumer<Juez>.SelectOne(apiUrl + id.ToString());
+            return View(data);
+        }
+
+        [Authorize(Roles = "Administrador")]
+        // POST: JuezController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Juez juez)
+        {
+            try
+            {
+                APIConsumer<Juez>.Delete(apiUrl + id.ToString());
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(juez);
+            }
+        }
+    }
+}
